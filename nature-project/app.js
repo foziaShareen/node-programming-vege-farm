3a61b9d3c8b619a1e529e7da73901092dd868b28
foziaShareen <foziashareen1@gmail.com>
2025-12-08 10:15:35 +0500

    write and assign new id (writefile)

--- nature-project/app.js
+++ nature-project/app.js
@@ -17,9 +17,20 @@ app.get('/api/v1/tours',(req,res)=>{
 })
 
 app.post('/api/v1/tours',(req,res)=>{
-    console.log(req.body)
+    //console.log(req.body)
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id:newId},req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
+        res.status(201).json({
+            status:"success",
+            data:{
+                tour:newTour
+            }
+        })
+    })
 
-    res.send('you have post data ')
+   // res.send('you have post data ')
 })
 app.listen(port,()=>{
  console.log(`server is running on port ${port}`)
